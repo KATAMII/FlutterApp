ba64cef3b568c0b263da5d4548f099a019619ed7
KATAMII <lydiamwaah01@gmail.com>
2024-10-10 21:55:40 +0300

    practice buttons,images,icons,stateless widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home:Scaffold(
-    appBar: AppBar(
-      title: Text('My first flutter App'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-          'welcome to Katamii first App ',
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-                color: Colors.lightGreen,
-                fontFamily: 'IndieFlower',
-              ),
+  home:Home(),
+));
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My first flutter App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child:ElevatedButton.icon(
+            onPressed: (){},
+            icon:Icon(
+          Icons.mail
+      ),
+            label: Text('Email')),
 
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: null, // Button will be disabled
-      child: Text('Click'), // Plain text without styling
-      backgroundColor: Colors.red[600],
-    ),
-  ),
-));
+      floatingActionButton: FloatingActionButton(
+        onPressed: null, // Button will be disabled
+        child: Text('Click'), // Plain text without styling
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
+
 
 
 
